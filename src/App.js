e6d0c95f531dc224cf172757fd82899fafd99d8f
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-05-27 10:41:19 +0530

    cleanup and navbar landing create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from "react";
 import './App.css';
 
-function App() {
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+
+const App = () => {
   return (
-    <div className="App">
-      <h3>hello</h3>
-    </div>
+    <>
+        <Navbar/>
+        <Landing/>
+    </>
   );
 }
 
