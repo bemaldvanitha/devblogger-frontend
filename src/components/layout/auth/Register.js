29afbfea95f6855b5848c1f281a51e58e92234de
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-05-28 11:03:01 +0530

    sign in created

--- src/components/layout/auth/Register.js
+++ src/components/layout/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import {Link} from "react-router-dom";
 
 const Register = () => {
     const [formData, setFormData] = useState({
@@ -14,7 +15,7 @@ const Register = () => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
     }
 
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault();
 
         if(password !== password2){
@@ -65,7 +66,7 @@ const Register = () => {
                 <input type="submit" className="btn btn-primary" value="Register"/>
             </form>
             <p className="my-1">
-                Already have an account? <a href="login.html">Sign In</a>
+                Already have an account? <Link to="/login">Sign In</Link>
             </p>
         </>
     )
