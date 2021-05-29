291469a52c452c98452faa45e05ad7ef62768a68
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-05-29 10:25:28 +0530

    alert6 actions types created

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
-const rootReducer = combineReducers({
+import alertReducer from "./alertReducer";
 
+const rootReducer = combineReducers({
+    alert: alertReducer,
 });
 
 export default rootReducer;
