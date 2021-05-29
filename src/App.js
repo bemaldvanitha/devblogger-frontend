3e979bc846debb58606cd5716dcdfad2c3c8d170
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-05-29 10:11:07 +0530

    store created and redux added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router , Route, Switch } from 'react-router-dom';
-import './App.css';
+import { Provider } from 'react-redux';
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/layout/auth/Login";
 import Register from "./components/layout/auth/Register";
+import store from "./store";
+
+import './App.css';
 
 const App = () => {
   return (
-    <Router>
-        <>
-            <Navbar/>
-            <Route exact path={'/'} component={Landing}/>
-            <section className={'container'}>
-                <Switch>
-                    <Route exact path={'/register'} component={Register}/>
-                    <Route exact path={'/login'} component={Login}/>
-                </Switch>
-            </section>
-        </>
-    </Router>
+      <Provider store={store}>
+        <Router>
+            <>
+                <Navbar/>
+                <Route exact path={'/'} component={Landing}/>
+                <section className={'container'}>
+                    <Switch>
+                        <Route exact path={'/register'} component={Register}/>
+                        <Route exact path={'/login'} component={Login}/>
+                    </Switch>
+                </section>
+            </>
+        </Router>
+      </Provider>
   );
 }
 
